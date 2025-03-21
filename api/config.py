7d3eb2ce1e87e0c3a45cb3370a3aeb5c115d8cfe
Subash Giri <mrsbs.giri@gmail.com>
2025-03-21 22:35:47 +0545

    fix: env sample

--- api/config.py
+++ api/config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-SECRET_KEY = os.getenv("SECRET_KEY")
-APP_URL = os.getenv("APP_URL")
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
+APP_URL = os.environ.get("APP_URL")
